efb7304b2258dcbbe59e1d663f1ae0d8ba30d824
Léo GASPEROWICZ <leo.gasperowicz@edu.devinci.fr>
2024-05-09 18:47:31 +0200

    update

--- Carrom/Connection/MariaDB.cs
+++ Carrom/Connection/MariaDB.cs
@@ -3,52 +3,220 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using MySqlConnector;
+using MySqlX.XDevAPI.Common;
+using Npgsql.Replication;
 
 namespace Carrom
 {
-    public class MariaDB : Connection
+    public class MariaDB
     {
-        private MySqlConnection connection;
+        //private MySqlConnection connection;
+        string MyConnectionString = "server=127.0.0.1;user=root;password=1234;port=3307";
+
 
         public MariaDB()
         {
-            string connectionString = "server=localhost;port=3306;database=nom_de_votre_base;user=votre_utilisateur;password=votre_mot_de_passe";
-            connection = new MySqlConnection(connectionString);
+            MySqlConnection connection = new MySqlConnection(MyConnectionString);
         }
 
-        public override void openConnection()
+
+        public void testConnection()
         {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(MyConnectionString);
+                connection.Open();
+                var stm = "SELECT VERSION()";
+                var cmd = new MySqlCommand(stm, connection);
+
+                var version = cmd.ExecuteScalar().ToString();
+                Console.WriteLine($"MariaDB version: {version}");
+            }
+            catch (Exception ex) { }
+
+        }
+        public void createDB()
+        {
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = "CREATE DATABASE IF NOT EXISTS `Carrom`;";
+                var response = cmd.ExecuteNonQuery();
+                Console.WriteLine($"Response: {response}");
+            }
+        }
+        public void tryCreateAlterTable()
+        {
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = "USE Carrom";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "DROP TABLE IF EXISTS player";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "CREATE TABLE IF NOT EXISTS player (id INT AUTO_INCREMENT    PRIMARY KEY, name VARCHAR(255), password VARCHAR(100))";
+                var response = cmd.ExecuteNonQuery();
+                Console.WriteLine($"Response: {response}");
+
+                //cmd.CommandText = "ALTER TABLE player ADD COLUMN NumVictory VARCHAR(255)";
+                //response = cmd.ExecuteNonQuery();
+                //Console.WriteLine($"Response: {response}");
+            }
+        }
+        public void createScoreTable()
+        {
+            string retour = "";
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = "USE Carrom;";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "DROP TABLE IF EXISTS score;";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = @"
+            CREATE TABLE IF NOT EXISTS score (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                idP int,
+                date TIME,
+                score INT,
+                CONSTRAINT fk_username FOREIGN KEY (idP) REFERENCES player(id)
+            );";
+                cmd.ExecuteNonQuery();
+                retour = "Score table created successfully.";
+            }
+        }
+        public void createUser(string name, string password)
+        {
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = "USE Carrom";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "INSERT INTO player(name,password) VALUES(@person, @mdp)";
+                cmd.Parameters.AddWithValue("@person", name);
+                cmd.Parameters.AddWithValue("@mdp", password);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+        }
+        public bool isExist(string name)
+        {
+            bool exist = false;
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                try
+                {
+                    conn.Open();
+                    cmd.CommandText = "USE Carrom";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "SELECT name FROM player WHERE name = @person";
+                    cmd.Parameters.AddWithValue("@person", name);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        exist = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    // handle exception here
+                }
+                finally
+                {
+
+                    conn.Close();
+                }
+            }
+            return exist;
+        }
+        public List<string> listTable() 
+        {
+            List<string> allPlayers = new List<string>();
+            var connection = new MySqlConnection(MyConnectionString);
+            using (var cmd = connection.CreateCommand())
             try
             {
                 connection.Open();
-                Console.WriteLine("Successful connection to MariaDB");
+                cmd.CommandText = "USE Carrom";
+                cmd.ExecuteNonQuery();
+                MySqlCommand command = new MySqlCommand("SELECT name FROM player", connection);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                            // access your record colums by using reader
+                            allPlayers.Add(reader["name"].ToString());
+                    }
+                }
             }
-            catch (MySqlException ex)
+            catch (Exception ex)
             {
-                Console.WriteLine($"Error opening connection to MariaDB: {ex.Message}");
+                // handle exception here
             }
-        }
-
-        public override void closeConnection()
-        {
-            try
+            finally
             {
                 connection.Close();
-                Console.WriteLine("Connection to MariaDB closed successfully");
             }
-            catch (MySqlException ex)
-            {
-                // Gérer l'exception
-                Console.WriteLine($"Error closing connection to MariaDB: {ex.Message}");
-            }
+            return allPlayers;  
         }
-
-        public override List<Player> queryUsers()
+        public void deletePlayer(string name)
         {
-            
-            // return a list of players
-            return new List<Player>();
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+                cmd.CommandText = "USE Carrom";
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "Delete from player where name = @name";
+                cmd.Parameters.AddWithValue("@name", name);
+                var response = cmd.ExecuteNonQuery();
+                Console.WriteLine($"Response: {response}");
+           }
+        }
+        public bool checkUser(string name, string password)
+        {
+            bool result = false;
+            using (var conn = new MySqlConnection(MyConnectionString))
+            using (var cmd = conn.CreateCommand())
+                try
+                {
+                    conn.Open();
+                    cmd.CommandText = "USE Carrom";
+                    cmd.ExecuteNonQuery();
+                    cmd.CommandText = "SELECT name FROM player where name = @name AND password = @password";
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            reader.Close();
+                            result = true;
+                        }
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    
+                    // handle exception here
+                }
+                finally
+                {
+                    
+                    conn.Close();
+                }
+            return result;
         }
     }
 }
--- Carrom/MainWindow.xaml.cs
+++ Carrom/MainWindow.xaml.cs
@@ -22,78 +22,142 @@ namespace Carrom
     /// 
     public partial class MainWindow : Window
     {
+        private MariaDB mariaDb;
         public MainWindow()
         {
+            mariaDb = new MariaDB();
+            mariaDb.createScoreTable();
             InitializeComponent();
-            //WindowState = WindowState.Maximized;
+            WindowState = WindowState.Maximized;
+
+            /*mariaDb.testConnection();
+            mariaDb.createDB();
+            mariaDb.tryCreateAlterTable();
+            mariaDb.createUser();
+            mariaDb.listTable();
+            mariaDb.deletePlayer();
+            mariaDb.checkUser();*/
+
         }
         private void PlayButtonClick(object sender, RoutedEventArgs e)
         {
             // Change the Grid
             StarterImage.Visibility = Visibility.Collapsed;
             PlayButton.Visibility = Visibility.Collapsed;
-            ConfigGrid.Visibility = Visibility.Visible;
+            ConfigGridP1.Visibility = Visibility.Visible;
         }
-
-        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        private void PassToPlayer2(object sender, RoutedEventArgs e)
         {
             // Verify that all the names are filled
-            bool arePlayerNamesFilled = !string.IsNullOrWhiteSpace(Player1TextBox.Text) &&
-                                            !string.IsNullOrWhiteSpace(Player2TextBox.Text) &&
-                                            Player1TextBox.Text != "Name Player 1" &&
-                                            Player2TextBox.Text != "Name Player 2";
-
-            // Verify that a BDD is selected
-            bool isDatabaseSelected = MariaDBRadioButton.IsChecked == true || PostgreSQLRadioButton.IsChecked == true;
-
-            if (arePlayerNamesFilled && isDatabaseSelected)
+            bool arePlayerNamesFilled = !string.IsNullOrWhiteSpace(NamePlayer1TextBox.Text) &&
+                                            !string.IsNullOrWhiteSpace(MdpPlayer1TextBox.Text) &&
+                                            NamePlayer1TextBox.Text != "Name Player 1" &&
+                                            MdpPlayer1TextBox.Text != "Password Player 1";
+            bool areWellLogin = mariaDb.checkUser(NamePlayer1TextBox.Text, MdpPlayer1TextBox.Text);
+            string player1Name = "";
+            if (arePlayerNamesFilled && areWellLogin)
             {
-                string player1Name = Player1TextBox.Text;
-                string player2Name = Player2TextBox.Text;
-                string databaseChoice = "";
-                if (MariaDBRadioButton.IsChecked == true)
-                {
-                    databaseChoice = "MariaDB";
-                }
-                else if (PostgreSQLRadioButton.IsChecked == true)
-                {
-                    databaseChoice = "PostgreSQL";
-                }
-                InitializeGame(player1Name, player2Name, databaseChoice);   
+                player1Name = NamePlayer1TextBox.Text;
+                string player1Password = MdpPlayer1TextBox.Text;
+                ConfigGridP1.Visibility = Visibility.Collapsed;
+                ConfigGridP2.Visibility = Visibility.Visible;
+                //InitializeGame(player1Name, player2Name, databaseChoice);   
             }
             else
             {
-                MessageBox.Show("Please fill in both player names and select a database to start the game.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                if (areWellLogin == false) // Becareful it remains the button to be created player 
+                {
+                    MessageBox.Show("There is an error in your name or in your password", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Please fill in the player name and the password to log in.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+        public void createPlayer(object sender, RoutedEventArgs e)
+        {
+            ConfigGridP1.Visibility = Visibility.Collapsed;
+            ConfigGridP2.Visibility = Visibility.Collapsed;
+            ConfigGridNP.Visibility = Visibility.Visible;
+        }
+        private void StartGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Verify that all the names are filled
+            bool arePlayerNamesFilled = !string.IsNullOrWhiteSpace(NamePlayer2TextBox.Text) &&
+                                            !string.IsNullOrWhiteSpace(MdpPlayer2TextBox.Text) &&
+                                            NamePlayer2TextBox.Text != "Name Player 2" &&
+                                            MdpPlayer2TextBox.Text != "Password Player 2";
+
+            // Verify that a BDD is selected
+            bool areWellLogin = mariaDb.checkUser(NamePlayer2TextBox.Text, MdpPlayer2TextBox.Text);
+
+            if (arePlayerNamesFilled && areWellLogin)
+            {
+                string player2Name = NamePlayer2TextBox.Text;
+                string player2Mdp = MdpPlayer2TextBox.Text;
+                ConfigGridP2.Visibility = Visibility.Collapsed;
+                GameGrid.Visibility = Visibility.Visible;
+                //InitializeGame(player1Name, player2Name, databaseChoice);   
+            }
+            else
+            {
+                if (areWellLogin == false)
+                {
+                    MessageBox.Show("There is an error in your name or in your password", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Please fill in the player name and the password to log in.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
             }
 
         }
-        public void PrintScore(Player player1, Player player2, Score scores )
+        public void PrintScore(Player player1, Player player2, Score scores)
         {
             Player1Score.Text = $"{player1.Name}: {scores.GetScoreById(player1.Id)}";
             Player2Score.Text = $"{player2.Name}: {scores.GetScoreById(player1.Id)}";
         }
         // To raise what is write in the textbox when a person click on it
-        private void Player1TextBox_GotFocus(object sender, RoutedEventArgs e)
+        private void NamePlayer1TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
             if (tb.Text == "Name Player 1")
             {
                 tb.Text = "";
-                tb.Foreground = Brushes.Gray; 
+                tb.Foreground = Brushes.Gray;
             }
         }
-
-        // To fill the textbox when it is null
-        private void Player1TextBox_LostFocus(object sender, RoutedEventArgs e)
+        private void NamePlayer1TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
             if (string.IsNullOrWhiteSpace(tb.Text))
             {
                 tb.Text = "Name Player 1";
-                tb.Foreground = Brushes.Gray; 
+                tb.Foreground = Brushes.Gray;
             }
         }
-        private void Player2TextBox_GotFocus(object sender, RoutedEventArgs e)
+
+        // To fill the textbox when it is null
+        private void MdpPlayer1TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "Password Player 1")
+            {
+                tb.Text = "";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void MdpPlayer1TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "Password Player 1";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void NamePlayer2TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
             if (tb.Text == "Name Player 2")
@@ -102,7 +166,7 @@ namespace Carrom
                 tb.Foreground = Brushes.Gray;
             }
         }
-        private void Player2TextBox_LostFocus(object sender, RoutedEventArgs e)
+        private void NamePlayer2TextBox_LostFocus(object sender, RoutedEventArgs e)
         {
             TextBox tb = (TextBox)sender;
             if (string.IsNullOrWhiteSpace(tb.Text))
@@ -111,7 +175,124 @@ namespace Carrom
                 tb.Foreground = Brushes.Gray;
             }
         }
-        
+        private void MdpPlayer2TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "Password Player 2")
+            {
+                tb.Text = "";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void MdpPlayer2TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "Password Player 2";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void NamePlayerNPTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "Choose a name")
+            {
+                tb.Text = "";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void NamePlayerNPTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "Choose a name";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void MdpPlayerNPTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "Write your password")
+            {
+                tb.Text = "";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void MdpPlayerNPTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "Write your password";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void CMdpPlayerNPTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (tb.Text == "Confirm your password")
+            {
+                tb.Text = "";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void CMdpPlayerNPTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(tb.Text))
+            {
+                tb.Text = "Confirm your password";
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+        private void goBackToLogin(object sender, RoutedEventArgs e)
+        {
+            bool arePlayerNamesFilled = !string.IsNullOrWhiteSpace(NamePlayerNPTextBox.Text) &&
+                                            !string.IsNullOrWhiteSpace(MdpPlayerNPTextBox.Text) &&
+                                            !string.IsNullOrWhiteSpace(CMdpPlayerNPTextBox.Text) &&
+                                            NamePlayerNPTextBox.Text != "Choose a name" &&
+                                            MdpPlayerNPTextBox.Text != "Write your password" &&
+                                            CMdpPlayerNPTextBox.Text != "Confirm your password";
+            bool passwordAreEquals = (MdpPlayerNPTextBox.Text == CMdpPlayerNPTextBox.Text);
+            // Verify that the player doesn't exist
+            bool exist = mariaDb.isExist(NamePlayerNPTextBox.Text);
+
+            if (arePlayerNamesFilled && exist == false && passwordAreEquals)
+            {
+                mariaDb.createUser(NamePlayerNPTextBox.Text, MdpPlayerNPTextBox.Text);
+                ConfigGridNP.Visibility = Visibility.Collapsed;
+                ConfigGridP1.Visibility = Visibility.Visible;
+                //InitializeGame(player1Name, player2Name, databaseChoice);   
+            }
+            else
+            {
+                if (exist == true)
+                {
+                    MessageBox.Show("This player is already register", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else if (passwordAreEquals == false)
+                {
+                    MessageBox.Show("The 2 passwords that you writed aren't the same", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Please fill in the player name and the password to create a new player.", "Missing Information", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+        private void listPlayers(object sender, EventArgs e)
+        {
+            ListPlayers.Visibility = Visibility.Visible;
+            List<string> allPlayers = mariaDb.listTable();
+            PlayersListBox.ItemsSource = allPlayers;
+        }
+        private void goBackToLogin2(object sender, RoutedEventArgs e)
+        {
+            ListPlayers.Visibility = Visibility.Collapsed;
+            ConfigGridP1.Visibility = Visibility.Visible;
+        }
     }
 }
 
